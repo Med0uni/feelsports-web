79ac5a8e48d4319eed27cd92ade20080f060e5b8
Med0uni <medouni13@gmail.com>
2024-12-10 17:50:20 +0100

    Disable image optimization + output:export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['images.unsplash.com'],
   },
-  basePath: '/feelsports-web', // Replace with your GitHub repository name
+  basePath: '/feelsports-web', 
   assetPrefix: '/feelsports-web',
+  images: {
+    unoptimized: true,
+  }
 }
 
 export default nextConfig
