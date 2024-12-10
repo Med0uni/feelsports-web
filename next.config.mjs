b38c105ed408c2545dd0ca32b8f2768a184efa9b
Med0uni <medouni13@gmail.com>
2024-12-10 18:29:03 +0100

    Images

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 const nextConfig = {
   output: 'export', // Keeps your build static for GitHub Pages or other static hosting
   basePath: '/feelsports-web', // Ensures all routes are prefixed with the base path
-  assetPrefix: '/feelsports-web', // Automatically applies to static assets (e.g., images, JavaScript files)
+  assetPrefix: '/feelsports-web', // Automatically applies to static assets (like images, JavaScript files)
   images: {
     unoptimized: true, // Disable image optimization (important for static export)
+    loader: 'custom', // Custom loader for images
+    path: '/feelsports-web/', // Ensure the path for images includes the base path
     domains: ['images.unsplash.com'], // Add external image domains if required
   },
 };
